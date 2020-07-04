7910527ee6311512dfa2ef61672a309fb4582fdd
iwkt <iwk@mac.com>
2020-07-04 21:45:58 +0900

    202007042145

---
+++ assets/js/img-zoom.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  function showElementAnimation() {
+    
+
+    let element = document.getElementsByClassName('cover_slide');
+    if (!element) return; // 要素がなかったら処理をキャンセル
+
+
+    let showTiming = window.innerHeight > 768 ? 180 : 50; // 要素が出てくるタイミングはここで調整
+    let scrollY = window.pageYOffset; //スクロール量を取得
+    let windowH = window.innerHeight; //ブラウザウィンドウのビューポート(viewport)の高さを取得
+
+    for (let i = 0; i < element.length; i++) {
+      let elemClientRect = element[i].getBoundingClientRect();
+      let elemY = scrollY + elemClientRect.top;
+      if (scrollY + windowH - showTiming > elemY) {
+        element[i].classList.add('inview');
+      } else if (scrollY + windowH < elemY) {
+        // 上にスクロールして再度非表示にする場合はこちらを記述
+        element[i].classList.remove('inview');
+      }
+    }
+  }
+  showElementAnimation();
+  window.addEventListener('scroll', showElementAnimation);
+
+});
