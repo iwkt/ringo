f5278c3b059a78d7d8143ab725a07144961d66c7
iwkt <iwk@mac.com>
2020-07-16 12:01:24 +0900

    202007161200

--- assets/js/humbger.js
+++ assets/js/humbger.js
@@ -1,5 +1,28 @@
+const mobilemenu = document.querySelector('#MobileMenu')
+const layor = document.querySelector('.layor');
+
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('navOpen').addEventListener('click', function () {
     this.classList.toggle('active');
+    // document.getElementById('MobileMenu').classList.toggle('active');
+    mobilemenu.classList.toggle('active');
+    layor.classList.toggle('active');
   })
 });
+
+
+// class MobileMenu {
+//   constructor() {
+//     this.DOM = {};
+//     this.DOM.btn = document.getElementById('navOpen');
+//     this.DOM.container = document.getElementById('MobileMenu')
+//     this.DOM.btn.addEventListener('click',this._toggle());
+//   }
+
+//   _toggle() {
+//       this.DOM.btn.classList.toggle('active');
+//       this.DOM.container.classList.toggle('active');
+//   }
+// }
+
+// new MobileMenu();
