2090962e4356f29e8e3aa167d4847982282ce503
iwkt <iwk@mac.com>
2020-07-24 22:04:33 +0900

    202007242204

--- assets/js/swiper_local.js
+++ assets/js/swiper_local.js
@@ -21,7 +21,7 @@ let slider1 = new Swiper('.slider1', {
     nextEl: '.swiper-button-next', //「次へボタン」要素の指定
     prevEl: '.swiper-button-prev', //「前へボタン」要素の指定
   }
-})
+});
 
 let slider2 = new Swiper('.slider2', {
   // 以下にオプションを設定
@@ -39,7 +39,25 @@ let slider2 = new Swiper('.slider2', {
     nextEl: '.swiper-button-next', //「次へボタン」要素の指定
     prevEl: '.swiper-button-prev', //「前へボタン」要素の指定
   }
-})
+});
+
+let slider_pc = new Swiper('.slider_pc', {
+  // 以下にオプションを設定
+  loop: true, //最後に達したら先頭に戻る
+
+  //ページネーション表示の設定
+  pagination: {
+    el: '.swiper-pagination', //ページネーションの要素
+    type: 'bullets', //ページネーションの種類
+    clickable: true, //クリックに反応させる
+  },
+
+  //ナビゲーションボタン（矢印）表示の設定
+  navigation: {
+    nextEl: '.swiper-button-next_pc', //「次へボタン」要素の指定
+    prevEl: '.swiper-button-prev_pc', //「前へボタン」要素の指定
+  }
+});
 
 
 
